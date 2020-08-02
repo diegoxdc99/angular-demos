9f5779eb9aacc73c8acb13284e8a4a52fe9c5155
diegoxdc99 <diegoxdc99@hotmail.com>
2020-08-02 13:53:55 -0500

    estilos acordion2

--- src/app/shared/components/accordion/accordion.component.ts
+++ src/app/shared/components/accordion/accordion.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, HostBinding, OnDestroy } from '@angular/core';
+import { Component, OnInit, HostBinding, OnDestroy, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
 import { accordionAnimations } from './accordion-animations';
 import { UniqueSelectionDispatcherService } from 'src/app/core/services/unique-selection-dispatcher.service';
 let counter = 0;
@@ -6,6 +6,7 @@ let counter = 0;
   selector: 'app-accordion',
   templateUrl: './accordion.component.html',
   styleUrls: ['./accordion.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
   animations: [accordionAnimations.bodyExpansion]
 })
 export class AccordionComponent implements OnInit, OnDestroy {
@@ -18,11 +19,13 @@ export class AccordionComponent implements OnInit, OnDestroy {
     if (expanded) {
       this.uniqueSelectionDispatcherService.notify(this.id);
     }
+    this._changeDetectorRef.markForCheck();
   }
   id = counter++;
 
   private _removeUniqueSelectionListener: () => void = () => {};
-  constructor(protected uniqueSelectionDispatcherService: UniqueSelectionDispatcherService) {
+  constructor(protected uniqueSelectionDispatcherService: UniqueSelectionDispatcherService,
+              private _changeDetectorRef: ChangeDetectorRef) {
     this._removeUniqueSelectionListener = uniqueSelectionDispatcherService.listen((id: number) => {
         if (this.id !== id) {
           this.isExpanded = false;
